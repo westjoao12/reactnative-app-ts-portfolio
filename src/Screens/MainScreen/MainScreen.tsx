3c8fd7ca41157f7d03f1eb96ff446d3f326b85ff
westjoao12 <westjoao12@gmail.com>
2025-09-04 03:16:19 +0100

    organização do setup

---
+++ src/Screens/MainScreen/MainScreen.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from 'react-native'
+import {styles} from './MainScreenStyle'
+
+export const MainScreen = () =>{
+    return(
+        <View style={styles.container}>
+            <Text>Main Screen</Text>
+        </View>
+    );
+}
---
+++ src/Screens/SkillScreen/SkillScreen.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import {styles} from './SkillScreenStyles';
+
+export const SkillScreen = () => {
+    return(
+        <View style={styles.container}>
+            <Text>Skill Screen...</Text>
+        </View>
+    );
+}
