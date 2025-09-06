09822449710682e888c6d222408ce1ad9f087e6f
westjoao12 <westjoao12@gmail.com>
2025-09-06 10:58:01 +0100

    create my own componet button

--- src/Screens/MainScreen/MainScreen.tsx
+++ src/Screens/MainScreen/MainScreen.tsx
@@ -1,10 +1,12 @@
 import {Text, View} from 'react-native'
 import {styles} from './MainScreenStyle'
+import { StatusBar } from 'expo-status-bar';
 
 export const MainScreen = () =>{
     return(
         <View style={styles.container}>
             <Text>Main Screen</Text>
+            <StatusBar style='auto'/>
         </View>
     );
 }
--- src/Screens/MainScreen/MainScreenStyle.tsx
+++ src/Screens/MainScreen/MainScreenStyle.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet } from "react-native";
 
-export const style = StyleSheet.create({
+export const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'blue'
