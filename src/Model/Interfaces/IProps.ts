93947326cf4d903d736c5ed82b1286423b565f1b
westjoao12 <westjoao12@gmail.com>
2025-09-06 11:21:00 +0100

    create interface contract to component

---
+++ src/Model/Interfaces/IProps.ts
@@ -0,0 +1,4 @@
+export interface IProps{
+    texto: string,
+    linkSocial: string
+}
---
+++ src/components/ButtonSocialMedia/ButtonSocialMedia.tsx
@@ -0,0 +1,11 @@
+import { Pressable, Text } from "react-native";
+import { IProps } from "../../Model/Interfaces/IProps";
+
+
+export const ButtonSocialMedia = (props: IProps)=>{
+    return(
+        <Pressable>
+            <Text><a href=""></a></Text>
+        </Pressable>
+    )
+}
