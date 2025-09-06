3e575576ca2ada4fd2f7d5a5da3949688545d71a
westjoao12 <westjoao12@gmail.com>
2025-09-06 12:26:47 +0100

    styling Button Component

--- src/components/ButtonSocialMedia/ButtonSocialMedia.tsx
+++ src/components/ButtonSocialMedia/ButtonSocialMedia.tsx
@@ -1,11 +1,16 @@
 import { Pressable, Text } from "react-native";
+import { styles } from "./ButtonSocialMediaStyles";
 import { IProps } from "../../Model/Interfaces/IProps";
 
 
 export const ButtonSocialMedia = (props: IProps)=>{
     return(
-        <Pressable>
-            <Text><a href=""></a></Text>
+        <Pressable style={styles.btn}>
+            <Text>
+                <a href={props.linkSocial}>
+                    {props.texto}
+                </a>
+            </Text>
         </Pressable>
     )
 }
--- src/components/ButtonSocialMedia/ButtonSocialMediaStyles.tsx
+++ src/components/ButtonSocialMedia/ButtonSocialMediaStyles.tsx
@@ -1,8 +1,25 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'blue'
+    btn: {
+        backgroundColor: 'rgba(31, 40, 51, 0.5)',
+        borderColor: '#45A29E',
+        borderWidth: 2,
+        elevation: 3,
+
+        width: '80%',
+        height: 50
+    },
+    txt: {
+        flex: 1,
+        fontSize: 25,
+        color: '#fff',
+        letterSpacing: 0.25,
+        textAlign:"center",
+        textAlignVertical: "center",
+    },
+    lnk:{
+        textDecorationLine:"none"
     }
 
 });
